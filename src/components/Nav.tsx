1902c9568c8a96bba010ea1b9800b953f92db7d4
lixiaohu123147 <7595644+lixiaohu123147@user.noreply.gitee.com>
2020-06-23 17:34:55 +0800

    创建 Icon 组件

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import React from 'react';
+import Icon from './Icon';
+
 
 const NavWrapper = styled.nav`
 line-height:24px;
@@ -10,22 +12,33 @@ box-shadow:0 0 3px rgba(0,0,0,0.25);
   >li{
     width:33.3333%;
     text-align:center;
-    padding:16px;
+    padding:4px 0;
+    display:flex;
+    flex-direction:column;
+    justify-content: center;
+    align-items: center;
+    .icon{
+     width:24px;
+     height:24px;
+    }
   }
 }
 `;
 
-const Nav = ()=>{
+const Nav = () => {
     return (
         <NavWrapper>
             <ul>
                 <li>
+                    <Icon name="tag"/>
                     <Link to="/tags">标签页</Link>
                 </li>
                 <li>
+                    <Icon name="money"/>
                     <Link to="/money">记账页</Link>
                 </li>
                 <li>
+                    <Icon name="chart"/>
                     <Link to="/statistics">统计页</Link>
                 </li>
             </ul>
