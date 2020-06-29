9c60acdbac72c9e53699107eed8c8d7721ff073e
lixiaohu123147 <7595644+lixiaohu123147@user.noreply.gitee.com>
2020-06-29 09:10:37 +0800

    收集四个组件的数据

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -20,9 +20,15 @@ const Wrapper = styled.section`
     margin-top: 8px;
   }
 `;
-const TagsSection: React.FC = (props) => {
+
+type Props = {
+  value: string[];
+  onChange: (selected: string[]) => void;
+}
+
+const TagsSection: React.FC <Props> = (props) => {
   const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
-  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  const selectedTags = props.value;
   const onAddTag = () => {
     const tagName = window.prompt('新标签的名称为');
     if (tagName !== null) {
@@ -33,9 +39,9 @@ const TagsSection: React.FC = (props) => {
     const index = selectedTags.indexOf(tag);
     if (index >= 0) {
       //如果tag已被选中，就复制所有没有被选中的tag，作为新的selectedTag
-      setSelectedTags(selectedTags.filter(t => t !== tag));
+      props.onChange(selectedTags.filter(t => t !== tag));
     } else {
-      setSelectedTags([...selectedTags, tag]);
+      props.onChange([...selectedTags, tag]);
     }
   };
   const getClass = (tag: string) => selectedTags.indexOf(tag) >= 0 ? 'selected' : '';
