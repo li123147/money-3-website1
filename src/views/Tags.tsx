df62598d3a1b1926779fc618a1750b0a93d003a6
lixiaohu123147 <7595644+lixiaohu123147@user.noreply.gitee.com>
2020-06-29 17:49:02 +0800

    tag list 样式做完

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,18 +1,58 @@
 import Layout from '../components/Layout';
-import React,{useState} from 'react';
+import React from 'react';
 import { useTags } from 'useTags';
+import styled from 'styled-components';
+import Icon from '../components/Icon';
+
+const TagList = styled.ol`
+font-size:16px;
+background: white;
+>li{
+    border-bottom:1px solid #d5d5d5;
+    line-height: 20px;
+    padding: 12px 16px 12px 0;
+    margin-left: 16px;
+    display:flex;
+    justify-content: space-between;
+    align-items: center;
+}
+`;
+const Button = styled.button`
+font-size:18px; border:none; padding:8px 12px;
+background: yellowgreen; border-radius:4px;
+color:white;
+`
+const Center = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items:center;
+    flex-direction:column;
+    
+`;
+const Space = styled.div`
+    height:16px;
+`
+
 
 function Tags() {
     const { tags, setTags } = useTags();
     return (
         <Layout>
-            <ol>
+            <TagList>
                 {tags.map(tag =>
                     <li key={tag}>
                         {tag}
+                        <Icon name="right"/>
                     </li>
                 )}
-            </ol>
+            </TagList>
+            <Center>
+                <Space />
+                <Space />
+                <Button>
+                    新增标签
+                </Button>
+            </Center>
         </Layout>
     );
 }
