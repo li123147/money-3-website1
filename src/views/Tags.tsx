6d983aa8b749266ee3b2a0ce33f0dbb5f97ac60b
lixiaohu123147 <7595644+lixiaohu123147@user.noreply.gitee.com>
2020-06-29 18:47:53 +0800

    创建 /tags/:tag 路由，学会 exact 的用法

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { useTags } from 'useTags';
 import styled from 'styled-components';
 import Icon from '../components/Icon';
+import { Link } from 'react-router-dom';
 
 const TagList = styled.ol`
 font-size:16px;
@@ -10,11 +11,13 @@ background: white;
 >li{
     border-bottom:1px solid #d5d5d5;
     line-height: 20px;
-    padding: 12px 16px 12px 0;
     margin-left: 16px;
+    > a {
+    padding: 12px 16px 12px 0;
     display:flex;
     justify-content: space-between;
     align-items: center;
+    }
 }
 `;
 const Button = styled.button`
@@ -31,7 +34,7 @@ const Center = styled.div`
 `;
 const Space = styled.div`
     height:16px;
-`
+`;
 
 
 function Tags() {
@@ -41,8 +44,10 @@ function Tags() {
             <TagList>
                 {tags.map(tag =>
                     <li key={tag}>
-                        {tag}
-                        <Icon name="right"/>
+                        <Link to={'/tags/' + tag}>
+                            <span className="onLine">{tag}</span>
+                            <Icon name="right" />
+                        </Link>
                     </li>
                 )}
             </TagList>
