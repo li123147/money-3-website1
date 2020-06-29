9b0e1e16dc397775f21842c80a83cb39432195e3
lixiaohu123147 <7595644+lixiaohu123147@user.noreply.gitee.com>
2020-06-29 21:41:32 +0800

    将 tag:string 重构为 tag:{id:number;name:string}

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -13,7 +13,7 @@ flex-direction:column;
 type Category = '-' | '+';
 function Money() {
     const [selected, setSelected] = useState({
-        tags: [] as string[],
+        tagIds: [] as number[],
         note: '',
         category: '-' as Category,
         amount: 0
@@ -23,10 +23,8 @@ function Money() {
     };
     return (
         <MyLayout >
-            <TagsSection value={selected.tags}
-                onChange={(tags) => onChange({
-                    tags
-                })} />
+            <TagsSection value={selected.tagIds}
+                onChange={tagIds => onChange({ tagIds })} />
             <NoteSection value={selected.note}
                 onChange={(note) => {
                     onChange({
