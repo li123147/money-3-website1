1d663952c4f6dc9d706a2dbaa814b5bb17d407e1
lixiaohu123147 <7595644+lixiaohu123147@user.noreply.gitee.com>
2020-06-30 10:22:17 +0800

    创建单独的 Input

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,9 +1,10 @@
-import Layout from '../components/Layout';
+import Layout from 'components/Layout';
 import React from 'react';
 import { useTags } from 'useTags';
 import styled from 'styled-components';
-import Icon from '../components/Icon';
+import Icon from 'components/Icon';
 import { Link } from 'react-router-dom';
+import { Button } from 'components/Button';
 
 const TagList = styled.ol`
 font-size:16px;
@@ -20,11 +21,6 @@ background: white;
     }
 }
 `;
-const Button = styled.button`
-font-size:18px; border:none; padding:8px 12px;
-background: yellowgreen; border-radius:4px;
-color:white;
-`
 const Center = styled.div`
     display: flex;
     justify-content: center;
@@ -35,7 +31,6 @@ const Center = styled.div`
 const Space = styled.div`
     height:16px;
 `;
-
 
 function Tags() {
     const { tags} = useTags();
