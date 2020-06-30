6f6d73d6c6cef908288c7b8d06db9a9fb716238e
lixiaohu123147 <7595644+lixiaohu123147@user.noreply.gitee.com>
2020-06-30 11:33:56 +0800

    编辑 tag 名

--- src/views/Money/NoteSection.tsx
+++ src/views/Money/NoteSection.tsx
@@ -1,24 +1,26 @@
 import styled from "styled-components";
-import React, { useRef, ChangeEventHandler } from 'react';
+import React, { ChangeEventHandler } from 'react';
 import { Input } from '../../components/Input';
 const Wrapper = styled.section`
 background:#f5f5f5;
-padding:0 16px;
+padding:14px 16px;
 font-size:14px;
 `;
 type Props = {
   value: string;
   onChange: (value: string) => void;
 }
-const NoteSection: React.FC <Props> = (props) => {
+const NoteSection: React.FC<Props> = (props) => {
   const note = props.value;
   const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
     props.onChange(e.target.value);
   };
   return (
     <Wrapper>
-      <Input label="备注" type="text" value={note} onChange={onChange}/>
-      
+      <Input label="备注" type="text" value={note} onChange={onChange}
+        placeholder="请填写备注"
+      />
+
     </Wrapper>
   )
 };
--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -5,6 +5,10 @@ import Layout from 'components/Layout';
 import Icon from 'components/Icon';
 import { Button } from 'components/Button';
 import styled from 'styled-components';
+import { Input } from '../components/Input';
+import { Center } from '../components/Center';
+import { Space } from '../components/Space';
+   
 
 type Params = {
     id: string
@@ -17,10 +21,15 @@ const Topbar = styled.header`
   padding: 14px;
   background:white;
 `;
-const Tag: React.FC = (props) => {
-    const { findTag } = useTags();
-    let { id } = useParams<Params>();
-    const tag = findTag(parseInt(id));
+const InputWrapper = styled.div`
+  background:white;
+  padding: 0 16px;
+  margin-top: 8px;
+`;
+const Tag: React.FC = () => {
+    const { findTag,updateTag } = useTags();
+    let { id:idString} = useParams<Params>();
+    const tag = findTag(parseInt(idString));
     return (
         <Layout>
             <Topbar>
@@ -28,15 +37,20 @@ const Tag: React.FC = (props) => {
                 <span>编辑标签</span>
                 <Icon />
             </Topbar>
-            <div>
-                <label>
-                    <span>标签名</span>
-                    <input type="text" placeholder="标签名" />
-                </label>
-            </div>
-            <div>
+            <InputWrapper>
+                <Input label="标签名" type="text" placeholder="标签名"
+                    value={tag.name}
+                    onChange={(e) => {
+                        updateTag(tag.id, { name: e.target.value });
+                    }}
+                />
+            </InputWrapper>
+            <Center>
+                <Space />
+                <Space />
+                <Space />
                 <Button>删除标签</Button>
-            </div>
+            </Center>
         </Layout>
     );
 };
